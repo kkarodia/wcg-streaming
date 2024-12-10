6c84e12af780f3aa524b1ed155e1f9fff232fc16
kkarodia <103606633+kkarodia@users.noreply.github.com>
2024-12-10 13:56:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
+import os
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
 try:
     import multiprocessing  # noqa
 except ImportError:
     pass
 
+# Determine PortAudio include and library paths
+portaudio_include_path = "/usr/include/portaudio2"
+portaudio_lib_path = "/usr/lib/x86_64-linux-gnu"
+
+# Check if paths exist, otherwise use system defaults
+if not os.path.exists(portaudio_include_path):
+    portaudio_include_path = "/usr/include"
+if not os.path.exists(portaudio_lib_path):
+    portaudio_lib_path = "/usr/lib"
+
 setuptools.setup(
     setup_requires=['pbr>=1.8'],
-    pbr=True)
-
-extra_compile_args = [
-    "-I/usr/include/portaudio2",
-    "-L/usr/lib/x86_64-linux-gnu",
-]
+    pbr=True,
+    # Add extra compilation arguments for PyAudio
+    ext_modules=[
+        setuptools.Extension(
+            'pyaudio._portaudio', 
+            sources=[],  # PyAudio will handle sources
+            include_dirs=[portaudio_include_path],
+            library_dirs=[portaudio_lib_path],
+            libraries=['portaudio'],
+            extra_compile_args=[
+                f"-I{portaudio_include_path}",
+                f"-L{portaudio_lib_path}"
+            ]
+        )
+    ]
+)
